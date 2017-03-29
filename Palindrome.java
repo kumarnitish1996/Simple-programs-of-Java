741788f08fd50e883c3ef658fbbe0978075b2add
Nitish Tiwari <kumarnitish1996@gmail.com>
2017-03-29 07:35:34 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,28 @@
+// This program is to check whether a given string is Palindrome or not
+// Date - 23/03/2017
+// By - Nitish Tiwari
+
+import java.util.Scanner;
+ 
+class Palindrome
+{
+   public static void main(String args[])
+   {
+      String original, reverse = "";
+      Scanner in = new Scanner(System.in);
+ 
+      System.out.println("Enter a string to check if it is a palindrome");
+      original = in.nextLine();
+ 
+      int length = original.length();
+ 
+      for ( int i = length - 1; i >= 0; i-- )
+         reverse = reverse + original.charAt(i);
+ 
+      if (original.equals(reverse))
+         System.out.println("Entered string is a palindrome.");
+      else
+         System.out.println("Entered string is not a palindrome.");
+ 
+   }
+}	
